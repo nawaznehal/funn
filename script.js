d80e6426be3a3582a1cbf475be9b789212604084
nawaznehal <50049028+nawaznehal@users.noreply.github.com>
2024-11-25 16:16:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "OMG YAYYY";
+  question.innerHTML = "OMG REALLYYY YAYYYY";
   gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
 });
 
-// Make the No button move randomly on hover
-noBtn.addEventListener("mouseover", () => {
+// Make the No button move randomly on hover or touch
+const moveNoButton = () => {
   const wrapper = document.querySelector(".wrapper");
   const wrapperRect = wrapper.getBoundingClientRect();
   const noBtnRect = noBtn.getBoundingClientRect();
@@ -24,4 +24,14 @@ noBtn.addEventListener("mouseover", () => {
 
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
+};
+
+// Listen for mouseover on desktop
+noBtn.addEventListener("mouseover", moveNoButton);
+
+// Listen for touch events on mobile
+noBtn.addEventListener("touchstart", (event) => {
+  // Prevent the default action (like scrolling) to ensure it only moves
+  event.preventDefault();
+  moveNoButton();
 });
